c7d24dd5bd37c0b35a589383e7723598858d7c70
AdnanNasifCloud <joseph.nasif@gmail.com>
2024-09-09 18:05:56 +0000

    10th

--- jellyfin-compose/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
+++ jellyfin-compose/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2024-09-08T21:11:04.3982795Z","EndTimeUtc":"2024-09-08T21:11:04.7168308Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
+{"StartTimeUtc":"2024-09-09T09:11:04.7168029Z","EndTimeUtc":"2024-09-09T09:11:04.7649071Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
