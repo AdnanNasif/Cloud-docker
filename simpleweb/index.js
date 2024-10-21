5c0d8fdc3d4c163cf542cbe1e5193b012dedb87d
AdnanNasifCloud <joseph.nasif@gmail.com>
2024-10-21 05:24:24 +0000

    21st oct

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hi there yolo asmr');
+  res.send('Hello World!yoloyolo');
 });
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080');
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
