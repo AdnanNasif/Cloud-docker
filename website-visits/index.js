dbf7dd365b78b440bdf3829b44ceb0a27ad35109
AdnanNasifCloud <joseph.nasif@gmail.com>
2024-10-28 05:52:29 +0000

    28th october

--- website-visits/index.js
+++ website-visits/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const redis = require('redis');
 
+
 const app = express();
 const client = redis.createClient({
   host: 'redis-server',
@@ -10,7 +11,7 @@ client.set('visits', 0);
 
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send('Number of visits ' + visits);
+    res.send('Number of visits ' + visits);                                                                                                                                                                                                                         
     client.set('visits', parseInt(visits) + 1);
   });
 });
